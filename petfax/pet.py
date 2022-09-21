9983be5385b0a9a6bfbe977855ba78f7db4960bb
Jaxon Johnson <88282807+WaffleKone@users.noreply.github.com>
2022-09-21 16:20:44 -0400

    updated index for navigation

--- petfax/pet.py
+++ petfax/pet.py
@@ -9,3 +9,8 @@ bp = Blueprint("pet", __name__, url_prefix="/pets")
 @bp.route('/')
 def index():
         return render_template('index.html', pets=pets)
+
+@bp.route('/<int:pet_id>')
+def show(pet_id):
+    pet = pets[pet_id - 1]
+    return render_template('pet.html', pet=pet)
